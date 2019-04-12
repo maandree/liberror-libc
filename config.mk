a2c0a557de306fe7a0db61065480146eee095278
Mattias Andrée <maandree@kth.se>
2019-04-12 21:43:15 +0200

    Use C99 rather than C11
    Signed-off-by: Mattias Andrée <maandree@kth.se>

--- config.mk
+++ config.mk
@@ -2,5 +2,5 @@ PREFIX    = /usr
 MANPREFIX = $(PREFIX)/share/man
 
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700
-CFLAGS   = -std=c11 -Wall -pedantic
+CFLAGS   = -std=c99 -Wall -pedantic
 LDFLAGS  = -s
